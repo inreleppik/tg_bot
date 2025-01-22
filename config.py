f6b2b52b90a34a034c1a93366f8002d2698f3629
Inre <115734990+inreleppik@users.noreply.github.com>
2025-01-22 20:47:07 +0300

    Add files via upload

--- config.py
+++ config.py
@@ -5,7 +5,7 @@ from dotenv import load_dotenv
 load_dotenv()
 
 # Чтение токена из переменной окружения
-TOKEN = os.getenv("8103299176:AAFV78XeN7llI2rX6thy4_BrWvT2wTTGSGE")
+TOKEN = os.getenv("BOT_TOKEN")
 
 if not TOKEN:
     raise ValueError("Переменная окружения BOT_TOKEN не установлена!")
